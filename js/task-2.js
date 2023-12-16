12441f5e9ea4648fb4250e18f8061b6ffaa35f12
VitaPad <v.mars22@ukr.net>
2023-12-16 20:30:17 +0200

    add index.html

--- js/task-2.js
+++ js/task-2.js
@@ -1,10 +1,13 @@
 function formatMessage(message, maxLength) {
-const message = "";
-let maxLength
-if (message <= maxLength) {
-    formatMessage = message;
+    if (message.length <= maxLength) {
+        return message;
+    } else {
+        return message.substring(0, maxLength) + "...";
+    }
 }
-    else (message > maxLength )
-    formatMessage = message.slice(0, maxLength) + "...";
-    return message
-}
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
